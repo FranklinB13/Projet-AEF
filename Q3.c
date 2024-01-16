13428c6c75083e208ad8bf40d78eea95df2d7bfc
IHE4 <153078249+IHE4@users.noreply.github.com>
2024-01-16 12:24:26 +0100

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -24,11 +24,11 @@ typedef struct {
 } Automate;
 
 
-int is_complete(Automaton *automaton) {
+int is_complete(Automate *automate) {
     for (int i = 0; i < ALPHABET_SIZE; i++) {
-        char letter = automaton->alphabet[i];
-        for (int j = 0; j < automaton->state_count; j++) {
-            if (is_transition_valid(automaton, letter, j) == -1) {
+        char letter = automate->alphabet[i];
+        for (int j = 0; j < automate->state_count; j++) {
+            if (is_transition_valid(automate, letter, j) == -1) {
                 return 0; // Automate non complet
             }
         }
